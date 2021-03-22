19787e3561d372e740555086d50c3a11ff26956e
mralext20 <mralext20@users.noreply.github.com>
2021-03-22 20:59:34 +0000

    deploy: c74bc9768a9c4a379aeff15d0af532be3151c8fa

--- js/main.js
+++ js/main.js
@@ -7789,7 +7789,7 @@
   });
 
   // ns-params:@params
-  var image = "/www.msbsdtechexpo.com/images/each.png";
+  var image = "/msbsdtechexpo.com/images/each.png";
 
   // js/main.js
   var leaflet = __toModule(require_leaflet_src());
